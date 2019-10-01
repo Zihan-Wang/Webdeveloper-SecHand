d92f9d2d467d2c5c889103d2ffcb85d73cf7d2f1
zihan wang <zihanwangbuaa@outlook.com>
2019-10-01 04:29:36 +0000

    change

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 
-app.listen(process.env.PORT = 3000, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("start");
 })
